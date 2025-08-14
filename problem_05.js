4a13047e79872d5a995d6d949e648a4749f58a8f
alamin-cse-20 <alamin.sust2020@gmail.com>
2025-08-14 08:59:48 +0600

    problem 05 solved

---
+++ problem_05.js
@@ -0,0 +1,14 @@
+function  resultReport( marks ) {
+    // You have to write your code here
+    if (Array.isArray(marks)) {
+        let n = marks.length;
+        let cnt = 0, sum = 0;
+        for (let i = 0; i < n; i++) {
+            sum += marks[i];
+            if (marks[i] >= 40) cnt++;
+        }
+        return { finalScore: Math.round(sum / n), pass: cnt, fail: n - cnt }
+    }
+    else return "Invalid";
+}
+console.log(resultReport([98, 87, 67, 91, 92, 33, 87]));
